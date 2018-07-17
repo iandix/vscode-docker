2cd729e24e34ea35969f5a3cb942b73ec5eefb7f
iandix <ianaguidinix@gmail.com>
2018-07-17 02:04:15 -0300

    Correction in launch-no-rm.sh

--- launch-no-rm.sh
+++ launch-no-rm.sh
@@ -1,8 +1,6 @@
-docker run -d \
+docker run \
     -e "DISPLAY=$DISPLAY" \
-    -e LOCAL_USER_ID=`id -u $USER` \
-    -e LOCAL_GROUP_ID=`id -g $USER` \
-    -e LOCAL_GROUP_NAME=`id -gn $USER` \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --mount source=learn-ros,target="/home/$USER/dev" \
-    --name vscode iandix/vscode
+    --mount source=learn-ros,target="/home/dev/workspaces" \
+    --name vscode \
+    iandix/vscode
